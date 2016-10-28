b0fe90387dbf26ec3e5fc3142498f4791ac26065
derek-bh <serve.light@prever.co.kr>
2016-10-28 17:52:15 +0900

    update .sh

--- pang-apps-monitor-mysqlmariadb/pang.sh
+++ pang-apps-monitor-mysqlmariadb/pang.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-JMX="-Djava.rmi.server.hostname=192.168.0.21"
-JMX="$JMX -Djava.net.preferIPv4Stack=true"
-JMX="$JMX -Dcom.sun.management.jmxremote.port=8123"
-JMX="$JMX -Dcom.sun.management.jmxremote.rmi.port=8123"
-JMX="$JMX -Dcom.sun.management.jmxremote=true"
-JMX="$JMX -Dcom.sun.management.jmxremote.local.only=false"
-JMX="$JMX -Dcom.sun.management.jmxremote.authenticate=false"
-JMX="$JMX -Dcom.sun.management.jmxremote.ssl=false"
-
-#To enable JMX
-#nohup $JMX java -cp ./libs/*:./conf com.pangdata.apps.monitor.MysqlMariaDBMonitor > /dev/null 2>&1&
-
 nohup java -cp ./libs/*:./conf com.pangdata.apps.monitor.MysqlMariaDBMonitor > /dev/null 2>&1&
